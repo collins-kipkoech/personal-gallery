60efd97cf5277b0d2f749136ef3a1a2e11ac8a00
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-13 12:48:29 +0300

    created the image model with image,name,description, location and category details

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pictures',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+cloudinary.config( 
+  cloud_name = "dtydg6xpa", 
+  api_key = "568552354739641", 
+  api_secret = "plPkmm4WcrylYnuoJxu3gSNNTuI" 
+)
--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+class Image(models.Model):
+  image = CloudinaryField('image')
+  image_name = models.CharField(max_length =60)
+  image_description = models.TextField()
+  image_location = models.ForeignKey(Location)
+  image_category = models.ForeignKey(Category)
