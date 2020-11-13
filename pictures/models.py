b55788b7cc403c721e804ec33ab05e387d32d3c2
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-13 15:37:23 +0300

    created the superuser account

--- pictures/models.py
+++ pictures/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from cloudinary.models import CloudinaryField
+# Create your models here.
 
+    
 class Image(models.Model):
-  image = CloudinaryField('image')
-  image_name = models.CharField(max_length =60)
-  image_description = models.TextField()
-  image_location = models.ForeignKey(Location)
-  image_category = models.ForeignKey(Category)
+    image = CloudinaryField('image')
+    
+
