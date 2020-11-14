6fc6c1d70b560fa2780f5e2abab9263794620ed4
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-14 17:55:35 +0300

    added details template to display details of each image

--- image/models.py
+++ image/models.py
@@ -27,17 +27,17 @@ class Location(models.Model):
 
 class Image(models.Model):
     
-    image_name = models.CharField(max_length=60)
+    image_title = models.CharField(max_length=60)
     image_description = models.TextField()
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     location = models.ForeignKey(Location,on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.image_name
+        return self.image_title
 
     
     class Meta:
-        ordering = ['image_name']
+        ordering = ['image_title']
 
     def get_absolute_url(self):
         return reverse('images:images_details',args=[self.id,])
