5830bfeb1b8519a91fc4b7bbef7bf74497449763
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-20 01:59:42 +0300

    fixing errors

--- image/views.py
+++ image/views.py
@@ -10,26 +10,16 @@ def index(request,location_slug=None):
     if location_slug:
         location = get_object_or_404(Location,slug=location_slug)
         images = images.filter(location=location)
-
     
     return render(request,'index.html',{'locations':locations,location:location,'images':images})
 
-
-
-
-
 def search_category(request):
-    
+
     if request.method == 'GET':
         search = request.GET.get('search')
-        category = Category.objects.all().filter(name=search)
+        categories = Category.objects.all().filter(name=search)
         
-        
-        return render(request,'search.html',{'category':category })
-
-# def details(request,image_id):
-#     image = get_object_or_404(Image,pk=image_id)
-#     return render(request,"image_details.html",{'image':image})
+        return render(request,'search.html',{'categories':categories })
 
 def details(request,image_id):
     try:
